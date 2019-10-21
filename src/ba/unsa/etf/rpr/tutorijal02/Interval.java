ab870278e03be8157e2eed881aeec3075231bcbb
lhodzic2 <lhodzic2@etf.unsa.ba>
2019-10-21 18:42:10 +0200

    Initial commit

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,33 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    double pocetnaTacka,krajnjaTacka;
+    boolean pocetnaPripadaIntervalu,krajnjaPripadaIntervalu;
 
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pocetnaPripadaIntervalu, boolean krajnjaPripadaIntervalu) {
+        if (krajnjaTacka > pocetnaTacka ) throw new IllegalArgumentException();
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pocetnaPripadaIntervalu = pocetnaPripadaIntervalu;
+        this.krajnjaPripadaIntervalu = krajnjaPripadaIntervalu;
+    }
 
+    public Interval() {
+        this.pocetnaTacka = 0;
+        this.krajnjaTacka = 0;
+        pocetnaPripadaIntervalu = false;
+        krajnjaPripadaIntervalu = false;
+
+    }
+
+//    public boolean isIn(double v) {
+  //  }
+
+    public boolean isNull() {
+        if (pocetnaTacka == 0 && krajnjaTacka == 0 && pocetnaPripadaIntervalu == false && krajnjaPripadaIntervalu == false) return true;
+        return false;
+    }
+
+    public Interval intersect(Interval interval) {
+    }
 }
